24820db9e22a4789e8267085b4bc9bf5f3473ec3
Fatih Furkan USTA <fatihfurkanusta@outlook.com>
2023-11-30 01:17:02 +0300

    Update Constant.java
    hide the open ai api

--- src/main/java/com/speechmaster/SpeechMaster/constant/Constant.java
+++ src/main/java/com/speechmaster/SpeechMaster/constant/Constant.java
@@ -2,5 +2,5 @@ package com.speechmaster.SpeechMaster.constant;
 
 public class Constant {
     public static final String OPEN_AI_URL = "https://api.openai.com/v1/completions";
-    public static final String OPEN_AI_KEY = "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";
+    public static final String OPEN_AI_KEY = "sk-";
 }
